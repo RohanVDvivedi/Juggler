790d1bb1d30eb199867e849c63eb86b2d497ced5
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-24 22:35:12 +0530

    you need to deinitialize that response object that the framework provided you, but you failed ot use

--- src/controller/load_balancing_controller.c
+++ src/controller/load_balancing_controller.c
@@ -12,6 +12,7 @@ int load_balancing_controller(HttpRequest* hrq, HttpResponse* hrp)
 	
 	job* promise = send_request_async(http_server_to_use, hrq, "juggler.lb");
 	HttpResponse* hrp_received = wait_or_get_response(promise, NULL);
+	deinitHttpResponse(hrp);
 	*hrp = *hrp_received;
 	free(hrp_received);
 
